80144d54eabe9827b6501f0d6be7b3dfe396c8c9
Asadbek2911 <new@mail.com>
2024-08-03 18:55:42 +0500

    inital

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -4,7 +4,7 @@ import OverviewNav from '../../components/OverviewNav';
 
 function Contacts() {
   return (
-    <div className='flex justify-between px-[32px] pt-5'>
+    <div className='sticky top-0 bg-[#d6dcfd] z-10 flex justify-between px-[32px] py-[15px] pt-5  rounded-b-[15px]'>
     <h1 className='text-[#252733] text-[24px] font-bold  leading-[30px]'>Contacts</h1>
     <OverviewNav/>
   </div> 
--- src/pages/Ticket/Tickets.jsx
+++ src/pages/Ticket/Tickets.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import OverviewNav from '../../components/OverviewNav';
+import Cards from '../../components/Cards';
 
 
 function Tickets() {
   return (
-    <div className='flex justify-between px-[32px] pt-5'>
+    <>
+    <div className='sticky top-0 bg-[#d6dcfd] z-10 flex justify-between px-[32px] py-[15px] pt-5  rounded-b-[15px]'>
     <h1 className='text-[#252733] text-[24px] font-bold  leading-[30px]'>Tickets</h1>
     <OverviewNav/>
   </div>
+  <Cards/>
+    </>
+
   )
 }
 
